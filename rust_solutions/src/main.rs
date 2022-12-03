b4002415ffb8ea673065f480fc2acdbae2748fd4
samboozle <samuel.a.bates@gmail.com>
2022-12-03 11:51:54 -0800

    just added a newline

--- rust_solutions/src/main.rs
+++ rust_solutions/src/main.rs
@@ -10,7 +10,7 @@ fn main() {
             |(i, f)| match f(format!("assets/d{}full.txt", i + 1).as_str()) {
                 Ok((sol1, sol2)) => {
                     println!(
-                        "Day {} solutions:\n    Part 1: {}\n    Part 2: {}\n",
+                        "\nDay {} solutions:\n    Part 1: {}\n    Part 2: {}\n",
                         i + 1,
                         sol1,
                         sol2
