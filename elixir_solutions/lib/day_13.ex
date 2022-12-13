5caecaf1f818f703eb705f14f63c36df558ec243
samboozle <samuel.a.bates@gmail.com>
2022-12-12 22:12:53 -0800

    legibility improvements

--- elixir_solutions/lib/day_13.ex
+++ elixir_solutions/lib/day_13.ex
@@ -10,10 +10,7 @@ defmodule Day13 do
     |> Enum.reduce([], fn pair, acc ->
       [
         String.split(pair, "\n", trim: true)
-        |> Enum.map(fn s ->
-          {list, _} = Code.eval_string(s)
-          list
-        end)
+        |> Enum.map(&elem(Code.eval_string(&1), 0))
         | acc
       ]
     end)
@@ -24,7 +21,7 @@ defmodule Day13 do
     packets
     |> Enum.with_index()
     |> Enum.reduce([], fn {[left, right], idx}, acc ->
-      case compare_packet_pair({left, right}) do
+      case compare_packet_pair(left, right) do
         true -> [idx + 1 | acc]
         false -> acc
       end
@@ -35,7 +32,7 @@ defmodule Day13 do
   def solution_2(packets) do
     packets
     |> Enum.reduce([[[2]], [[6]]], fn [a, b], acc -> [a, b | acc] end)
-    |> Enum.sort(fn a, b -> compare_packet_pair({a, b}) end)
+    |> Enum.sort(fn a, b -> compare_packet_pair(a, b) end)
     |> Enum.with_index()
     |> Enum.reduce(1, fn
       {[[2]], idx}, acc -> acc * (idx + 1)
@@ -44,25 +41,25 @@ defmodule Day13 do
     end)
   end
 
-  defp compare_packet_pair({[], _}), do: true
-  defp compare_packet_pair({[_ | _], []}), do: false
+  defp compare_packet_pair([], _), do: true
+  defp compare_packet_pair([_ | _], []), do: false
 
-  defp compare_packet_pair({[a | as], [a | bs]}),
-    do: compare_packet_pair({as, bs})
+  defp compare_packet_pair([a | as], [a | bs]),
+    do: compare_packet_pair(as, bs)
 
-  defp compare_packet_pair({[a | _], [b | _]})
+  defp compare_packet_pair([a | _], [b | _])
        when is_integer(a) and is_integer(b),
        do: a < b
 
-  defp compare_packet_pair({[a | _], [b | _]})
+  defp compare_packet_pair([a | _], [b | _])
        when is_integer(a) and is_list(b),
-       do: compare_packet_pair({[a], b})
+       do: compare_packet_pair([a], b)
 
-  defp compare_packet_pair({[a | _], [b | _]})
+  defp compare_packet_pair([a | _], [b | _])
        when is_list(a) and is_integer(b),
-       do: compare_packet_pair({a, [b]})
+       do: compare_packet_pair(a, [b])
 
-  defp compare_packet_pair({[a | _], [b | _]})
+  defp compare_packet_pair([a | _], [b | _])
        when is_list(a) and is_list(b),
-       do: compare_packet_pair({a, b})
+       do: compare_packet_pair(a, b)
 end
